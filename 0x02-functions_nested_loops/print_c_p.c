d8f596fa68fdbbed608623cbf940cd0c0ea69b70
cPicton <pictonchris2@gmail.com>
2021-09-03 20:17:49 +0300

    my initial commit

---
+++ 0x02-functions_nested_loops/print_c_p.c
@@ -0,0 +1,24 @@
+/** Author - Chris Picton
+*/
+#include <stdio.h>
+
+/**
+ * main - print letters between my favorite initials, c to p.
+ *
+ * Return: Always 0.
+ */
+
+ int main(void)
+
+ {
+     char cp;
+
+     cp = 'c';
+
+while (cp <= 'p')
+
+{putchar(cp);
+cp++;
+ }
+putchar('\n');
+ }

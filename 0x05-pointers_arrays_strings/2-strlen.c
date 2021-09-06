8cc7d4c2b8701fe2c424aeee05f4bdb3677f2aa8
cPicton <pictonchris2@gmail.com>
2021-09-06 13:27:31 +0300

    string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_strlen - finds and outputs length of string.
+ *@s: pointer to string.
+ *
+ *Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+const char *s
+for (s = str; *s; ++s);
+return(s - str);
+}

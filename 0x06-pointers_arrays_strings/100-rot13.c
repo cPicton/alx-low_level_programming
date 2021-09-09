8f2c4f02267ef851f1231032e0f68d2537f8e178
cPicton <pictonchris2@gmail.com>
2021-09-09 15:47:58 +0300

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,36 @@
 #include "main.h"
 
 /**
-* rot13 - encodes a string
-* Return: string
+* rot13 - encode string using rot13
+* @s: string to encode
+* Return: encoded string
 */
 
-char *rot13(char *y)
+char *rot13(char *s)
 {
-int i, j;
-char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-for (i = 0; y[i] != '\0'; i++)
+int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+'W', 'X', 'Y', 'Z'};
+int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+'J', 'K', 'L', 'M'};
+
+int i, j;
+
+for (j = 0; s[j] != '\0'; j++) /*loop through string*/
 {
-for (j = 0; a[j] != '\0'; j++)
-{
-if (y[i] == a[j])
-{
-y[i] == b[j];
-break;
+i = 0;
+while (a[i] != '\0' && s[j] != a[i]) /*loop through rot13 arr*/
+i++;
+
+if (s[j] == a[i]) /*if alpha matches, set to index in b arr*/
+s[j] = b[i];
 }
-}
-}
-return (y);
+
+return (s);
 }

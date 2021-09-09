42c9f266d481ee56424c69c2150360f5c386b2fd
cPicton <pictonchris2@gmail.com>
2021-09-09 23:35:54 +0300

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,28 +1,18 @@
 #include "main.h"
-
 /**
-* _strncpy - Copies a string
-*
-* @dest: Destination to copy to
-*
-* @src: Source to copy from
-*
-* @n: Index checker
+* _strncpy - function copies a string
+* @dest: string to be copied into
+* @src: string to be copied
+* @n: portion of src to be copied into dest
+* Return: returns a string - dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int i = 0;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
-{
 dest[i] = src[i];
-}
-
-for (; i < n; i++)
-{
 dest[i] = '\0';
-}
-
 return (dest);
 }

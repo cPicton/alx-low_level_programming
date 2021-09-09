98effad1cffb18e8c1b46bdb728a738329fb9b5f
cPicton <pictonchris2@gmail.com>
2021-09-09 14:16:36 +0300

    change characters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * string_toupper - converts all lowercase letters of a string to uppercase.
- * @c: pointer to the string to convert
- * Return: the converted string.
- */
+* string_toupper - converts all lowercase letters of a string to uppercase.
+* @c: pointer to the string to convert
+* Return: the converted string.
+*/
 
 char *string_toupper(char *x)
 {

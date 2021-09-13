76511862d0566fce5135c1b859d958716e7a9bb8
cPicton <pictonchris2@gmail.com>
2021-09-13 13:06:31 +0300

    locate substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,10 +2,10 @@
 #define NULL 0
 
 /**
-* _strstr - locate and return pointer to first occurence of substring
-* @haystack: string to search
-* @needle: target substring to search for
-* Return: pointer to index of string at first occurence of whole substring
+*   _strstr - locate and return pointer to first occurence of substring
+*    @haystack: string to search
+*     @needle: target substring to search for
+*  Return: pointer to index of string at first occurence of whole substring
 */
 
 char *_strstr(char *haystack, char *needle)
@@ -15,9 +15,10 @@ int i = 0, j, x;
 if (needle[0] == '\0')
 return (haystack);
 
-while (haystack[i] != '\0') 
+while (haystack[i] != '\0') /* iterate through haystack */
 {
-
+/* if a byte matches first char of needle */
+/* interate through needle until match ends */
 if (haystack[i] == needle[0])
 {
 x = i, j = 0;
@@ -26,8 +27,8 @@ while (needle[j] != '\0')
 if (haystack[x] == needle[j])
 x++, j++;
 else
-break;
-} 
+			break;
+} /* if matched throughout, return haystack */
 if (needle[j] == '\0')
 {
 return (haystack + i);
@@ -35,5 +36,5 @@ return (haystack + i);
 }
 i++;
 }
-return (NULL); 
+return (NULL); /* No match */
 }

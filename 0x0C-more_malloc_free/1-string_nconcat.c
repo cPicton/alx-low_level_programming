213e415c55c77c25d8ca06c34dbc06fdb0827f51
cPicton <pictonchris2@gmail.com>
2021-09-23 22:30:24 +0300

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,14 +1,15 @@
 #include "main.h"
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
 * string_nconcat - concatenates s1 to the first n bytes of s2
-* @s1 - string to be  received
-* @s2 - string
-* @n - bytes to be copied from s2
+* @s1: string to be  received
+* @s2: string
+* @n: bytes to be copied from s2
 *
-* Return: Pointer to the resulting string
+* Return: pointer to resulting string
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -18,30 +19,22 @@ unsigned int a, b, len1, len2;
 
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
-
 len1 = strlen(s1);
 len2 = strlen(s2);
-
 if (n >= len2)
 n = len2;
-
-neow = malloc(sizeof(*neow) * (len1 + n + 1));
+new = malloc(sizeof(*neow) * (len1 + n + 1));
 if (neow == NULL)
 return (NULL);
-
 for (a = 0; s1[a] != '\0'; a++)
 neow[a] = s1[a];
-
-for (b = 0; b < n &&  s2[b] != '\0'; b++)
+for (b = 0; b < n && s2[b] != '\0'; b++)
 {
 neow[a] = s2[b];
 a++;
 }
-
 neow[a] = '\0';
-
 return (neow);
 }

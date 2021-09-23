12f034e23e5af82e067a1a71e33a1454d023ca0b
cPicton <pictonchris2@gmail.com>
2021-09-24 01:36:50 +0300

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,55 +1,39 @@
 #include "main.h"
+#include <string.h>
 #include <stdlib.h>
-/**
-* _strlen - calculate and return string length
-* @string: string
-* Return: string length
-*/
-
-int _strlen(char *string)
-{
-int i;
-
-for (i = 0; string[i] != '\0'; i++)
-;
-return (i);
-}
 
 /**
-* string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
-* @s1: string 1
+* string_nconcat - concatenates s1 to the first n bytes of s2
+* @s1: stringto be  received
 * @s2: string 2
-* @n: n bytes to concat from string 2
-* Return: pointer to concatenated string
+* @n: bytes to be copied from s2
+*
+* Return -  pointer to resulting string
 */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr;
-int num, len, i, j;
-
-num = n;
+char *neow;
+unsigned int a, b, len1, len2;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (num < 0)
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+neow = malloc(sizeof(*neow) * (len1 + n + 1));
+if (neow == NULL)
 return (NULL);
-if (num >= _strlen(s2))
-num = _strlen(s2);
-
-len = _strlen(s1) + num + 1;
-
-ptr = malloc(sizeof(*ptr) * len);
-if (ptr == NULL)
-return (NULL);
-
-for (i = 0; s1[i] != '\0'; i++)
-ptr[i] = s1[i];
-for (j = 0; j < num; j++)
-ptr[i + j] = s2[j];
-ptr[i + j] = '\0';
-
-return (ptr);
+for (a = 0; s1[a] != '\0'; a++)
+neow[a] = s1[a];
+for (b = 0; b < n && s2[b] != '\0'; b++)
+{
+neow[a] = s2[b];
+a++;
+}
+neow[a] = '\0';
+return (neow);
 }

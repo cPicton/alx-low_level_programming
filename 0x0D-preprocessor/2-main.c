d3890ffb7770563ee4e937f9c53fe4e96ecf14cc
cPicton <pictonchris2@gmail.com>
2021-09-27 18:21:11 +0300

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file it was compiled from
-* return: void always
+* main - prints current filename to std output
+* Return: 0 on success
 */
 
 int main(void)
 {
 printf("%s\n", __FILE__);
-
 return (0);
 }

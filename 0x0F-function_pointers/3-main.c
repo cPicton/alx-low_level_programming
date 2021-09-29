7262ebe3b71f106f3718edc9675f448537eaaaed
cPicton <pictonchris2@gmail.com>
2021-09-29 19:12:30 +0300

    reach that goal!

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,40 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+
 /**
-* main - checks arguments.
-* @argc: argument count.
-* @argv: argument vector.
-*
-* Return: error if number of arguments is wrong.
+* main - simple operations program
+* @argc: argument counter
+* @argv: argument array
+* Return: 0 mostly
 */
+
 int main(int argc, char *argv[])
 {
-int a = 0, b = 0, c = 0;
-char p;
+int i, j, k;
+int (*f)(int, int);
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-
-if (argv[2][1] != '\0')
+i = atoi(argv[1]);
+j = atoi(argv[3]);
+f = get_op_func(argv[2]);
+if (f == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-
-p = argv[2][0];
-if (p != '+' && p != '-' && p != '/' && p != '*' && p != '%')
-{
-printf("Error\n");
-exit(99);
-}
-
-a = atoi(argv[1]);
-b = atoi(argv[3]);
-c = (get_op_func(argv[2]))(a, b);
-printf("%d\n", c);
+k = f(i, j);
+printf("%d\n", k);
 return (0);
 }

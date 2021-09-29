f7d4e995ac9b169a6dff6cc9f9bf575b570129c3
cPicton <pictonchris2@gmail.com>
2021-09-29 22:49:10 +0300

    reach that goal!

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,11 @@
 #include "3-calc.h"
+
 /**
-* get_op_func - selects correct function to perform.
-* @s: operator used.
-*
+* get_op_func - selects the correct function to perform the operation
+* @s: operator
 * Return: int
 */
+
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
@@ -16,15 +17,13 @@ op_t ops[] = {
 {NULL, NULL}
 };
 int i;
-i = 0;
 
-while (i < 5)
+i = 0;
+while (ops[i].op != NULL)
 {
-if (s[0] == ops[i].op[0])
-{
-return (ops[i].f);
-}
+if (strcmp(s, ops[i].op) == 0)
+break;
 i++;
 }
-return (NULL);
+return (ops[i].f);
 }

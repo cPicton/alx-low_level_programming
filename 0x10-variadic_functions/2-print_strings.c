2e36f1303e0accc1e7b82cad158fd0c612652abc
cPicton <pictonchris2@gmail.com>
2021-09-30 22:28:54 +0300

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -6,7 +6,7 @@
 * print_strings - prints string followed by a new line
 * @separator - string to be printed between the strings
 * @n - number of strings passed to the function
-
+*/
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list arg;

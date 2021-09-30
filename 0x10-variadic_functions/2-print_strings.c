7b5d753380fe066d609205b22db30c90fb2576cb
cPicton <pictonchris2@gmail.com>
2021-09-30 22:38:33 +0300

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
-/**
-* print_strings - prints string followed by a new line
-* @separator - string to be printed between the strings
-* @n - number of strings
-* Return: void
-*
-*/
-void print_strings(const char *separator, const unsigned int n, ...)
 
+/**
+* print_strings - print varying amount of strings
+* @separator: delimiter
+* @n: amount of arguments in list
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list valist;
 unsigned int i;

53c74116ab53fc8c3f836bef8aa0cbae18796c9c
cPicton <pictonchris2@gmail.com>
2021-10-07 20:37:26 +0300

    before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+* before_main - prints before main function is executed
+*/
 void  __attribute__((constructor))  before_main()
 {
 printf("You're beat! and yet, you must allow,\n");

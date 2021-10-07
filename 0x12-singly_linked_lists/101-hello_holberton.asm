3864c03bcf3f2ee01e02ce884dec1c15f6ebacbe
cPicton <pictonchris2@gmail.com>
2021-10-07 21:32:43 +0300

    nasm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,12 @@
-global start
+global main
 	section .text
-start:	mov rax,1
+main:	mov rax,1
 	mov rdi, 1
-	mov rsi, message
+	mov rsi, msg
 	mov rdx, 17
 	syscall
 	mov rax, 60
 	xor rdi,rdi
 	syscall
 	section .rodata
-message:	db "Hello, Holberton", 10, 0
+msg:	db "Hello, Holberton", 10, 0

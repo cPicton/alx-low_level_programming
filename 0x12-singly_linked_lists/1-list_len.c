43aa354dcd323807979fc5e35e8c968a3887b09a
cPicton <pictonchris2@gmail.com>
2021-10-07 18:26:10 +0300

    return element

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* list_len - returns the number of elements in a linked list
+* Return: number of elements
+*/
+
+size_t list_len(const list_t *h)
+
+{
+
+size_t elcount = 0;
+
+while (h != NULL)
+{
+elcount++;
+h = h->next;
+}
+
+return (elcount);
+}

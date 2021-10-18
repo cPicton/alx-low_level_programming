9252e79f9a5c1ad073474be07a51c863cee49cdb
cPicton <pictonchris2@gmail.com>
2021-10-18 23:37:17 +0300

    read text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,42 +9,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; /* file descriptor */
+	int cp; /* file decriptor*/
 	ssize_t n_read, n_wrote;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	/* open */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	cp = open(filename, O_RDONLY);
+	if (cp == -1)
 		return (0);
 
-	/* malloc buffer */
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	/* read */
-	n_read = read(fd, buffer, letters);
+	n_read = read(cp, buffer, letters);
 	if (n_read == -1)
 	{
 		free(buffer);
-		close(fd);
+		close(cp);
 		return (0);
 	}
 
-	/* write */
+
 	n_wrote = write(STDOUT_FILENO, buffer, n_read);
 	if (n_wrote == -1)
 	{
 		free(buffer);
-		close(fd);
+		close(cp);
 		return (0);
 	}
 
-	close(fd);
+	close(cp);
 	return (n_read);
 
 }

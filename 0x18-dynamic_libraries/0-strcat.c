7875fe665e9c2759cdfed28d6b399940f6c7e0a2
cPicton <pictonchris2@gmail.com>
2021-11-30 01:28:54 +0300

    dynamic libraries

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: string appended
+ * @src: string to be appended
+ * Return: appended string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: string appended
+ * @src: string to be appended to dest
+ * @n: size of concarenated strings in bytes
+ * Return: resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+}
